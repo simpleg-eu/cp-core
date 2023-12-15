930b08c5c2e643b4f432cc58916d38be8af2a9d1
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-12-15 23:46:16 +0100

    f incorrect parameters type

--- src/macros.rs
+++ src/macros.rs
@@ -4,10 +4,15 @@
 use crate::error::Error;
 #[macro_export]
 macro_rules! ok_or_return_error {
-    ($expression: expr, $error_kind: ident, $error_message: ident) => {
+    ($expression: expr, $error_kind: expr, $error_message: expr) => {
         match $expression {
             Ok(value) => value,
-            Err(error) => return Error::new($error_kind, format!("{}{}", $error_message, error)),
+            Err(error) => {
+                return Err(Error::new(
+                    $error_kind,
+                    format!("{}{}", $error_message, error),
+                ))
+            }
         }
     };
 }
