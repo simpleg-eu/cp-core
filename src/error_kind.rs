9c74cb8af8c865ccd7d040a9d8464476a8909171
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-01-19 12:37:12 +0100

    + `request_failure`& `timed_out`

--- src/error_kind.rs
+++ src/error_kind.rs
@@ -4,3 +4,5 @@
 
 pub const SERIALIZATION_FAILURE: &str = "serialization_failure";
 pub const SECRETS_MANAGER_FAILURE: &str = "secrets_manager_failure";
+pub const REQUEST_FAILURE: &str = "request_failure";
+pub const TIMED_OUT: &str = "timed_out";
